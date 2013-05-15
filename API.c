a00e94cf939d26c333fa22d073aa08ea8dc63e43
Armando <armandosoto100@gmail.com>
2013-05-15 10:47:34 -0300

    Version 1.0

---
+++ API.c
@@ -0,0 +1,67 @@
+#include "API.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Data {
+    u32 vertex_left;
+    u32 vertex_right;
+    u32 capacity;
+}
+
+
+struct DragonSt {
+    
+};
+
+typedef DragonSt *DragonP;
+
+
+DragonP NuevoDragon() {
+    DragonP dragon = calloc(1, sizeof(struct DragonSt));
+    
+}
+
+
+int DestruirDragon(DragonP N) {
+    
+}
+
+int CargarUnLado(DragonP N, u32 x, u32 y, u32 c) {
+    
+}
+
+int LlenarDragon(DragonP N) {
+    
+}
+
+int ECAML(DragonP N) {
+    
+}
+
+int DondeEstamosParados(DragonP N) {
+    
+}
+
+u32 AumentarFlujo(DragonP N) {
+    
+}
+
+u32 AumentarFlujoYtambienImprimirCamino(DragonP N) {
+    
+}
+
+void ImprimirFlujo(DragonP N) {
+    
+}
+
+void ImprimirValorFlujo(DragonP N) {
+    
+}
+
+void ImprimirCorte(DragonP N) {
+    
+}
+
+u64 Sumar64(u64 a, u32 b) {
+    
+}
